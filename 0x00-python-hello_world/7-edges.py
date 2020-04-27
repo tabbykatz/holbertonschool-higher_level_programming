3d3b794bbc114da22eaad1e80640d59128e4cb4b
tabbykatz <tomelay@gmail.com>
2020-04-27 17:05:06 +0000

    Task 7

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+word = "Holberton"
+word_first_3 = "Hol"
+word_last_2 = "on"
+middle_word = "olberto"
+print("First 3 letters: {}".format(word_first_3))
+print("Last 2 letters: {}".format(word_last_2))
+print("Middle word: {}".format(middle_word))
