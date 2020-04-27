dc70063028b0ded5e10ebad26b31f02f059e26b9
tabbykatz <tomelay@gmail.com>
2020-04-27 17:15:36 +0000

    Task 8

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,1 +1,5 @@
 #!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+language that combines remarkable power with very clear syntax"
+str = str[39:66] + " " + str[106:111] + str[:6]
+print(str)
