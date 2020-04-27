92db0fc7537e1b2c7f5e551354553fbca0639f7c
tabbykatz <tomelay@gmail.com>
2020-04-27 20:25:27 +0000

    remove hidden space

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
 language that combines remarkable power with very clear syntax"
-str = str[39:66] + str[105:111] + str[:7]
+str = str[39:66] + str[105:111] + str[:6]
 print(str)
