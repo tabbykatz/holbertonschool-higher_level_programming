1904d04efae78b961ccab04663a8e8d3296c617b
tabbykatz <tomelay@gmail.com>
2020-04-28 21:19:36 +0000

    remove import math as I didn't use it

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 import random
-import math
 number = random.randint(-10000, 10000)
 dig = number % 10 if number > 10 else number % -10
 print("Last digit of {:d} is {:d} and is "
