ab1d890076af598f2a9bf719afc98739000b9f4b
tabbykatz <tomelay@gmail.com>
2020-04-28 19:23:53 +0000

    remove abs function

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 import random
+import math
 number = random.randint(-10000, 10000)
-print("The last digit of {} is".format(number), end=' ')
-number = abs(number)
-if (number % 10) == 0:
-    print("{}, and is 0".format(number % 10))
-elif (number % 10) < 6:
-    print("{}, and is less than 6 and not 0".format(number % 10))
-elif (number % 10) > 5:
-    print("{}, and is greater than 5".format(number % 10))
+dig = number % 10 if number > 10 else number % -10
+print("Last digit of {:d} is {:d} and is "
+      .format(number, dig), end="")
+if dig > 5:
+    print("greater than 5")
+elif dig == 0:
+    print("0")
+else:
+    print("less than 6 and not 0")
