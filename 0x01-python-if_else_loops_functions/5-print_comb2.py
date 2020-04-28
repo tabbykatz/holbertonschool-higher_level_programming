2835c6a64459ba301eb3f45e392452bb1b41e6f3
tabbykatz <tomelay@gmail.com>
2020-04-28 19:26:26 +0000

    add second digit to single digit nums

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(100):
     if i < 99:
-        print("{:d},".format(i), end=' ')
+        print("{:02d},".format(i), end=' ')
     elif i == 99:
         print("{:d}".format(i))
