c555e9e1f762ec78c6671ef392e7320272877b10
tabbykatz <tomelay@gmail.com>
2020-04-30 19:59:47 +0000

    Task 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-import sys
+if __name__ == "__main__":
+    import sys
 
 answer = 0
 
-if len(sys.argv) == 1:
-    print("{:d}".format(len(sys.argv) - 1))
-if len(sys.argv) >= 2:
-    for i in range(1, len(sys.argv)):
-        answer += int(sys.argv[i])
-    print("{:d}".format(answer))
+for i in range(1, len(sys.argv)):
+    answer += int(sys.argv[i])
+print("{:d}".format(answer))
