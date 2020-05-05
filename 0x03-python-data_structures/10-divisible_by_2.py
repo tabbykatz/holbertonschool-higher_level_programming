982449c19cc2fba3b35d06a4e77a4b40d90d25de
tabbykatz <tomelay@gmail.com>
2020-05-05 20:51:32 +0000

    Rework completeley using append

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,8 @@
-#!/usr/bin/python3
-def divisible_by_2(my_list=[]):
-    new_list = my_list.copy()
-    for i in range(len(my_list)):
-        if (my_list[i] % 2 == 0):
-            new_list[i] = "True"
+sible_by_2(my_list=[]):
+    new_list = []
+    for i in my_list:
+        if i % 2 == 0:
+            new_list.append(True)
         else:
-            new_list[i] = "False"
+            new_list.append(False)
     return new_list
