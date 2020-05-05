b45d12a3e29710bb041bf46387e469a5c3293cb4
tabbykatz <tomelay@gmail.com>
2020-05-05 22:40:21 +0000

    work in progress

---
+++ 0x03-python-data_structures/for_arthur/13-is_palindrome.c
@@ -0,0 +1,51 @@
+#include "lists.h"
+/**
+ * reverse_list - reverse a linked list
+ * @head: points to list
+ * Return: addy of new head
+ */
+listint_t *reverse_list(listint_t **head)
+{
+	listint_t *prev = NULL, *next;
+
+	while (*head != NULL)
+	{
+		next = (*head)->next;
+		(*head)->next = prev;
+		prev = *head;
+		*head = next;
+	}
+
+	*head = prev;
+	return (*head);
+}
+/**
+ * is_palindrome - tests if sll is a palindrome
+ * @head: points to list
+ * Return: 1 if palindrome, 0 if not
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *tortoise = *head, *hare = *head, *one, *two;
+
+	while (hare != NULL && hare->next != NULL)
+	{
+		hare = hare->next->next;
+		one = tortoise;
+		tortoise = tortoise->next;
+	}
+
+	tortoise = reverse_list(&tortoise);
+	two = tortoise;
+	hare = *head;
+	while (hare && tortoise)
+	{
+		if (hare->n != tortoise->n)
+			return (0);
+		hare = hare->next;
+		tortoise = tortoise->next;
+	}
+	tortoise = reverse_list(&two);
+	one->next = tortoise;
+	return (1);
+}
--- 0x03-python-data_structures/for_arthur/lists.h
+++ 0x03-python-data_structures/for_arthur/lists.h
@@ -1,6 +1,9 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -11,8 +14,8 @@
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
@@ -20,5 +23,6 @@ listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 
 int is_palindrome(listint_t **head);
+listint_t *reverse_list(listint_t **head);
 
 #endif /* LISTS_H */
