5cd79fa33ba147b0103f7eb21fd9895a22f4e871
tabbykatz <tomelay@gmail.com>
2020-05-05 21:10:27 +0000

    Add True or False instead

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,8 +1,6 @@
+#!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    new_list = []
+    new = []
     for i in my_list:
-        if i % 2 == 0:
-            new_list.append(True)
-        else:
-            new_list.append(False)
-    return new_list
+        new = new + ([True] if not i % 2 else [False])
+    return new
