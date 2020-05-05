ea1a2051e27397ee6c8e0289cecb1e2e558c9ede
tabbykatz <tomelay@gmail.com>
2020-05-05 20:31:31 +0000

    check for not my_list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    i = len(my_list)
-    for i in reversed(range(i)):
-        print("{:d}".format(my_list[i]))
+    if not my_list:
+        return None
+    for i in reversed(my_list):
+        print("{:d}".format(i))
