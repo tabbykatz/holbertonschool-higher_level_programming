634b1d80c480c9d5f55f968c949ff5c4b1808137
tabbykatz <tomelay@gmail.com>
2020-05-05 20:25:24 +0000

    count properly

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx > 0 and idx <= len(my_list):
-        return my_list[idx]
-    else:
+    if idx < 0 or idx >= len(my_list):
         return None
+    return my_list[idx]
