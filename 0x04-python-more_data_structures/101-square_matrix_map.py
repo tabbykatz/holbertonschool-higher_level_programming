877b48bfeef0ba7cfbe05d558c78630dc3b978bb
tabbykatz <tomelay@gmail.com>
2020-05-07 21:15:15 +0000

    Fix errors caused by indent

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return list(map(lambda inside: list(map(lambda item: item**2, inside))
-                matrix))
+    return list(map(lambda i: list(map(lambda e: e**2, i)), matrix))
