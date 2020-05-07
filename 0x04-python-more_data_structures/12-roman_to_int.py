ca616c6f30b7e020ea612354c69659d5bf14998e
tabbykatz <tomelay@gmail.com>
2020-05-07 22:15:21 +0000

    remove excess var

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,6 @@ def roman_to_int(roman_string):
     if not roman_string or type(roman_string) != str:
         return 0
     total = 0
-    num = 0
     digits = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
     for roman in reversed(roman_string):
         arabic = digits[roman]
