7a4262d1e3a08707f5d3d119a8732ac156723239
tabbykatz <tomelay@gmail.com>
2020-05-18 22:43:13 +0000

    Task 3, making s None

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,7 +3,7 @@ def safe_print_division(a, b):
     try:
         a = a / b
     except ZeroDivisionError:
-        print("None")
+        a = None
         return None
     finally:
         print("Inside result:", "{}".format(a))
