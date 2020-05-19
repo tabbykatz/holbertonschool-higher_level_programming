009e359b05bfebfa77211bcc6374f49dd2cadbc9
tabbykatz <tomelay@gmail.com>
2020-05-19 22:14:51 +0000

    Task 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""My square module"""
+
+
+class Square:
+    """defines a square"""
+
+    def __init__(self, size=0):
+        """Create a Square
+
+        Args: size: length of a side of Square
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """"The propery of size as the len of a side of Square
+
+        Raises:
+            TypeError: if size != int
+            ValueErrorr: if size < 0
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+    def area(self):
+        """Get the area of a Square
+
+        Returns: The size squared
+        """
+        return self.__size * self.__size
