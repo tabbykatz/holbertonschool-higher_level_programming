f7a22eb858e637ab035207bb78eec5f7807c99ec
tabbykatz <tomelay@gmail.com>
2020-05-20 02:43:07 +0000

    adjust spacing

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -6,18 +6,17 @@ import math
 class MagicClass:
     """set up the magic"""
 
+def __init__(self, radius=0):
+    """ writing another docstring """
+    self.__radius = 0
+    if type(radius) is not int and type(radius) is not float:
+            raise TypeError('radius must be a number')
+    self.__radius = radius
 
-    def __init__(self, radius=0):
-        """ writing another docstring """
-        self.__radius = 0
-        if type(radius) is not int and type(radius) is not float:
-                raise TypeError('radius must be a number')
-        self.__radius = radius
+def area(self):
+    """again with the docstring"""
+    return self.__radius ** 2 * math.pi
 
-    def area(self):
-        """again with the docstring"""
-        return self.__radius ** 2 * math.pi
-
-    def circumference(self):
-        """such docstring"""
-        return 2 * math.pi * self.__radius
+def circumference(self):
+    """such docstring"""
+    return 2 * math.pi * self.__radius
