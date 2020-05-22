dd96c47fbe16c25f8ad9d5c3227a696e1a8df418
tabbykatz <tomelay@gmail.com>
2020-05-22 00:17:35 +0000

    Task 4 & tests

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""the text_indentation module"""
+
+
+def text_indentation(text):
+    """puts two new lines after these characters in a string: ., :, and ?
+    Args:
+        text: the string to print
+    Raises:
+        TypeError: if text is not a string
+    """
+    if not isinstance(text, str):
+        raise TypeError('text must be a string')
+    for char in ".?:":
+        text = (char + "\n\n").join(
+            [sen.strip(" ") for sen in text.split(char)])
+    print(text, end='')
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/5-text_indentation.txt")
