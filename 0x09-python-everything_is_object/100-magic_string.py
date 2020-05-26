fe067bb82e41e581b9fe99045b640bcc1a6f1492
tabbykatz <tomelay@gmail.com>
2020-05-26 23:21:25 +0000

    Task 30

---
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def magic_string():
+    setattr(magic_string, "times", getattr(magic_string, "times", 0) + 1)
+    return "Holberton" + ", Holberton" * getattr(magic_string, "times", 0)
