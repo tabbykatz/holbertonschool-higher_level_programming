0ff2ba6fc8bbe11b8ae3e0d93fefe311b67579e8
tabbykatz <tomelay@gmail.com>
2020-06-03 01:06:53 +0000

    make pep8

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """module for MyInt, a class - non test cases needed"""
 
+
 class MyInt(int):
     """my int"""
     def __eq__(self, other):
@@ -10,4 +11,3 @@ class MyInt(int):
     def __ne__(self, other):
         """myint is a rebel, ne is =="""
         return int(self) == int(other)
-
