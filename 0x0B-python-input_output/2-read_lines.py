399385a1fa4a13233804152de77a5af373fa69ad
tabbykatz <tomelay@gmail.com>
2020-06-04 01:01:04 +0000

    add Task 2

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -1,2 +1,15 @@
 #!/usr/bin/python3
+"""read_lines module"""
+
+
 def read_lines(filename="", nb_lines=0):
+    """to read n lines from file, print entire thing if n <= 0 or > file"""
+    with open(filename, "r", encoding="utf-8") as content:
+        if not nb_lines:
+            print(content.read(), end="")
+            return
+        for eachline in content:
+            print(eachline, end="")
+            nb_lines -= 1
+            if not nb_lines:
+                break
