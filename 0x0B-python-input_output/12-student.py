8020c5ccb11e202c76a600f3821ef95b0f44e089
tabbykatz <tomelay@gmail.com>
2020-06-04 01:13:44 +0000

    make pep8

--- 0x0B-python-input_output/12-student.py
+++ 0x0B-python-input_output/12-student.py
@@ -13,7 +13,7 @@ class Student:
     def to_json(self, attrs=None):
         """get dict, names if only string"""
         if type(attrs) is list and all([type(a) == str for a in attrs]):
-            return {name: value for name, value in self.__dict__.items() if name
-                   in attrs}
+            return {name: value for name, value in self.__dict__.items()
+                    if name in attrs}
         else:
             return self.__dict__.copy()
