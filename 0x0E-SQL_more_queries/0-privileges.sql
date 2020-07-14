fa7b37be358e7da98bd735fea74371f162eb1097
tabbykatz <tomelay@gmail.com>
2020-07-14 17:48:13 +0000

    add Tasks 0 - 3

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,4 @@
+-- 0-privileges.sql
+-- lists all privileges of the MySQL users user_0d_1 and user_0d_2 
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+-- 1-create_user.sql
+-- creates the MySQL server user user_0d_1
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
