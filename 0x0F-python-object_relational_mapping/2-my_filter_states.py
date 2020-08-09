72ca12febb7bb08f4b7928e89b1bd0705ce3d187
tabbykatz <tomelay@gmail.com>
2020-08-09 17:01:50 +0000

    Return to injection risk

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -8,9 +8,8 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306, charset="utf8",
                            user=argv[1], passwd=argv[2], db=argv[3])
     cur = conn.cursor()
-    cur.execute(
-        "SELECT * FROM states WHERE name = %s ORDER BY states.id ASC",
-        (argv[4], ))
+    cur.execute("SELECT * FROM states WHERE states.name LIKE \"{}\"\
+                                ".format(argv[4]))
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
