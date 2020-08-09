ace4a1d47f5008606fc52a0e0dd339da751a155c
tabbykatz <tomelay@gmail.com>
2020-08-09 17:06:23 +0000

    corrected return

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""list all states that are argument, no injection risk"""
+""" print states starting with capital N"""
 
 import MySQLdb
 from sys import argv
@@ -8,8 +8,10 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306, charset="utf8",
                            user=argv[1], passwd=argv[2], db=argv[3])
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE states.name LIKE \"{}\"\
-                                ".format(argv[4]))
+    query = """ SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY
+        states.id ASC"""
+    query = query.format(argv[4])
+    cur.execute(query)
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
