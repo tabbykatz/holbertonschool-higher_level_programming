8738bed2957f1e29afeb2ec90e86e9448237762c
tabbykatz <tomelay@gmail.com>
2020-08-18 22:31:17 +0000

    return to mine

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,8 +1,27 @@
 #!/usr/bin/python3
-""" peaky """
+""" a function to find an element in list whose neighbors are both < it"""
 
 
 def find_peak(list_of_integers):
-    """peaky peaks"""
-    if list_of_integers:
-        return max(list_of_integers)
+    """ function using recursion """
+    nums = list_of_integers
+
+    if len(nums) == 0 or nums is None:
+        return None
+
+    if (len(nums) == 1):
+        return (nums[0])
+
+    if nums[0] > nums[1]:
+        return nums[0]
+    if nums[-1] > nums[-2]:
+        return nums[-1]
+
+    middle = len(nums) // 2
+
+    if (nums[middle - 1] > nums[middle]):
+        return(find_peak(nums[:middle]))
+    elif (nums[middle + 1] > nums[middle]):
+        return(find_peak(nums[middle + 1:]))
+    else:
+        return(nums[middle])
