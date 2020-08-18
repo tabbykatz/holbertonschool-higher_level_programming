0a7ed907c98ddf7306bcc1b8bd32771a0212b8d3
tabbykatz <tomelay@gmail.com>
2020-08-18 21:14:22 +0000

    Task 6

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" a function to find an element in list whose neighbors are both < it"""
+
+
+def find_peak(list_of_integers):
+    """ function using recursion """
+    nums = list_of_integers
+
+    if len(nums) == 0 or nums is None:
+        return None
+
+    if (len(nums) == 1):
+        return (nums[0])
+
+    middle = len(nums) // 2
+
+    if (nums[middle - 1] > nums[middle]):
+        return(find_peak(nums[:middle]))
+    elif (nums[middle + 1] > nums[middle]):
+        return(find_peak(nums[middle + 1:]))
+    else:
+        return(nums[middle])
