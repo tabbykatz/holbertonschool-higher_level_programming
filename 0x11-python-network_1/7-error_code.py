18b3de8183e00e9b6d58d826b5fae712776c710d
tabbykatz <tomelay@gmail.com>
2020-08-24 20:23:04 +0000

    make pep8

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """ sends a request to the URL and displays the response with requests """
-""" Uses requests module. Prddints error code"""
-import requests
 from sys import argv
+import requests
 
 if __name__ == "__main__":
     response = requests.get(argv[1])
