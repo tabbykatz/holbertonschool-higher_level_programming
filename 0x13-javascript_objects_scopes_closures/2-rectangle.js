56991638317b7ea6376ec90aefee91988bfddfa2
tabbykatz <tomelay@gmail.com>
2020-09-21 20:53:25 +0000

    Task 2

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// Rectangle class
+class Rectangle {
+  constructor (w, h) {
+    if ((w = parseInt(w)) > 0 &&
+        (h = parseInt(h)) > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
+module.exports = Rectangle;
