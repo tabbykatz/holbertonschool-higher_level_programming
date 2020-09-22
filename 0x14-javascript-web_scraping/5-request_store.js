4470673314f18fc439c7531950d0dc512050647d
tabbykatz <tomelay@gmail.com>
2020-09-22 21:32:42 +0000

    Task 5

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+//  gets the contents of a webpage and stores it in a file
+const fs = require('fs');
+const request = require('request');
+request(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
