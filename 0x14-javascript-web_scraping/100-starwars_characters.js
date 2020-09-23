90036b886e412fbb94e20d0b307114af97028132
tabbykatz <tomelay@gmail.com>
2020-09-23 17:33:24 +0000

    Task 7

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+// get starwars characters from a film 
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  if (error) throw error;
+  for (const character of JSON.parse(body).characters) {
+    request(character, function (error, response, body) {
+      if (error) throw error;
+      console.log(JSON.parse(body).name);
+    });
+  }
+});
+
