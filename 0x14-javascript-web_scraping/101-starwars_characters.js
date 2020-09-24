e1fa698a66d44dc792573823cd1e361a675d8306
tabbykatz <tomelay@gmail.com>
2020-09-24 19:15:16 +0000

    Task 8

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,24 +1,21 @@
 #!/usr/bin/node
-// retrieves all character names in SW film
+// get all star wars chars in the right order
+const args = (process.argv);
 const request = require('request');
-const url = 'http://swapi.co/api/films/' + process.argv[2];
-
-const first = function () {
-  request(url, function (error, response, body) {
-    if (error) throw error;
-    second(JSON.parse(body).characters, 0);
-  });
-};
-
-const second = function (characters, i) {
-  if (characters.length === i) {
-    return;
+const url = 'https://swapi-api.hbtn.io/api/films/' + args[2];
+request(url, function (error, response, body) {
+  if (!error) {
+    const chars = JSON.parse(body).characters;
+    printIt(chars, 0);
   }
-  request(characters[i], function (error, response, body) {
-    if (error) throw error;
-    console.log(JSON.parse(body).name);
-    second(characters, ++i);
+});
+function printIt (chars, i) {
+  request(chars[i], function (error, response, body) {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+      if (i + 1 < chars.length) {
+        printIt(chars, i + 1);
+      }
+    }
   });
-};
-
-first();
+}
